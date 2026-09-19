cff65a0752eaa1d4177757dbd6b5ad739d3eb42e
Ferrer007 <ferrercastillo2005@gmail.com0>
2026-09-19 12:40:01 -0500

    Initial commit: MS-Usuarios con Spring Boot

---
+++ src/main/java/com/nutrimax/msusuarios/MsUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.nutrimax.msusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUsuariosApplication.class, args);
+	}
+
+}
